935ac683d2119d233b2d9dc18c53d9c2ef20f268
@maticZvikart <zvikisan@gmail.com>
2021-02-18 18:28:27 +0100

    login logika

---
+++ login.php
@@ -0,0 +1,50 @@
+<?php
+    include_once "header.php";
+?>
+
+
+<section class="page-section" id="contact">
+    <div class="container">
+        <!-- Contact Section Heading-->
+        <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Prijava</h2>
+        <!-- Icon Divider-->
+        <div class="divider-custom">
+            <div class="divider-custom-line"></div>
+            <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+            <div class="divider-custom-line"></div>
+        </div>
+        <!-- Registration Section Form-->
+        <div class="row">
+            <div class="col-lg-8 mx-auto">
+                <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19.-->
+                <form action="login_check.php" method="post">
+                  
+                
+                    <div class="control-group">
+                        <div class="form-group floating-label-form-group controls mb-0 pb-2">
+                            <label>E-Pošta</label>
+                            <input class="form-control" type="email" name="email" placeholder="Vnesite e-pošto"
+                                required="required" /> <br />
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <div class="form-group floating-label-form-group controls mb-0 pb-2">
+                            <label>Geslo</label>
+                            <input class="form-control" type="password" name="pass" placeholder="Vnesite geslo"
+                                required="required" /> <br />
+                        </div>
+                    </div>
+                  
+                    <br />
+                            <div id="success"></div>
+                            <div class="form-group"><button class="btn btn-primary btn-xl" id="sendMessageButton" type="submit">Pošlji</button></div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php
+    include_once "footer.php";
+?>
---
+++ login_check.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+
+include_once "database.php";
+
+$email = $_POST['email'];
+$pass = $_POST['pass'];
+
+if (!empty($email) && !empty($pass))
+ {
+     $query = "SELECT * FROM users WHERE email = ?";
+$stmt = $pdo->prepare($query);
+$stmt->execute([$email]);
+
+if($stmt->rowCount() == 1)
+ {
+    $user = $stmt->fetch(); 
+
+    if (password_verify($pass,$user['pass']))
+    {
+        $_SESSION['logged']= 1;
+        header("Location: index.php");
+        die();
+    }
+ }
+}
+ 
+header("Location: login.php");
+die();
+
+?>
