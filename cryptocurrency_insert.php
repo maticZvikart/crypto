30c73b94b1a58228e9a7fd4601365fa85f394752
@maticZvikart <zvikisan@gmail.com>
2021-02-28 19:01:27 +0100

    nalaganje logotipov

--- cryptocurrency_insert.php
+++ cryptocurrency_insert.php
@@ -5,20 +5,57 @@ include_once "database.php";
 $title = $_POST['title'];
 $description = $_POST['description'];
 $current_price = floatval($_POST['current_price']);
-$logo = 'logotip';
 
-if  (!empty($title) && !empty($logo)) {
 
-    $query = "INSERT INTO cryptocurrencies(title, description, current_price, logo) VALUES (?,?,?,?)";
 
-    $stmt = $pdo->prepare($query);
-    $stmt->execute([$title,$description,$current_price,$logo]);
+$target_dir = "uploads/";
+$random = date('YmdHisu');
+$target_file = $target_dir . $random . basename($_FILES["logo"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-    header("Location: cryptocurrencies.php");
+//preveri ali ima datoteka dejansko velikost
+$check = getimagesize($_FILES["logo"]["tmp_name"]);
+if($check !== false) {
+  $uploadOk = 1;
+} else {
+  $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["logo"]["size"] > 5000000) {
+    $uploadOk = 0;
+    
+  }
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  $uploadOk = 0;
+}
+
+
+//preverim, ali so podatki polni in ustrezni
+if  (!empty($title) && ($uploadOk == 1)) {
+
+    if (move_uploaded_file($_FILES["logo"]["tmp_name"], $target_file)) {
+       //zapiše se vse v bazo
+       $query = "INSERT INTO cryptocurrencies(title, description, current_price, logo) VALUES(?,?,?,?)";
+       $stmt = $pdo->prepare($query);
+       $stmt->execute([$title,$description,$current_price,$target_file]);
+   
+       header("Location: cryptocurrencies.php");
+       die();
+
+      } else {
+        header("Location: cryptocurrency_add.php");
+        die();
+      }   
 }
 else 
 {
-    header("Location: cryptocurreny_add.php");
+    header("Location: cryptocurrency_add.php");
+    die();
 }
 
 
